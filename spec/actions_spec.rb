97ef5993e6ca17dc1a2a85a9283f04713e6a1352
GreyWayfarer <l.box@bk.ru>
2021-12-07 00:24:36 +0700

    Add actions_spec

---
+++ spec/actions_spec.rb
@@ -0,0 +1,77 @@
+require './lib/actions'
+
+RSpec.describe Actions do
+  describe '#execute!' do
+    context 'Go work' do
+      status = {
+        'health' => 100,
+        'mana' => 30,
+        'happienss' => 5,
+        'fatigue' => 0,
+        'money' => 100
+      }
+      go_work_action = {
+        'conds' => {
+          'mana' => {
+            'value' => '50',
+            'operator' => '<'
+          },
+          'fatigue' => {
+            'value' => '10',
+            'operator' => '<'
+          }
+        },
+        'effects' => {
+          'happienss' => {
+            'value' => '5',
+            'operator' => '-'
+          },
+          'mana' => {
+            'value' => '30',
+            'operator' => '-'
+          },
+          'money' => {
+            'value' => '100',
+            'operator' => '+'
+          },
+          'fatigue' => {
+            'value' => '70',
+            'operator' => '+'
+          }
+        }
+      }
+      expected = { 'fatigue' => 70, 'happienss' => 0, 'health' => 100, 'mana' => 0, 'money' => 200 }
+      it { expect(Actions.new.execute!(status, go_work_action)).to eq expected }
+    end
+    context 'Walk in the park' do
+      status = {
+        'health' => 100,
+        'mana' => 30,
+        'happienss' => 5,
+        'fatigue' => 0,
+        'money' => 100
+      }
+      walk_in_the_park_action = {
+        'conds' => {
+
+        },
+        'effects' => {
+          'happienss' => {
+            'value' => '1',
+            'operator' => '+'
+          },
+          'mana' => {
+            'value' => '10',
+            'operator' => '-'
+          },
+          'fatigue' => {
+            'value' => '10',
+            'operator' => '+'
+          }
+        }
+      }
+      expected = { 'fatigue' => 10, 'happienss' => 6, 'health' => 100, 'mana' => 20, 'money' => 100 }
+      it { expect(Actions.new.execute!(status, walk_in_the_park_action)).to eq expected }
+    end
+  end
+end

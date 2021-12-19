bc2f58ee1107b0b7c1f3e8f4079232d060fbf3fe
Ant2000s <skrebnev.anton@gmail.com>
2021-12-20 02:49:09 +0700

    update tests save_load

--- spec/save_load_spec.rb
+++ spec/save_load_spec.rb
@@ -13,10 +13,10 @@ RSpec.describe Saver do
         }
       end
       let(:file) { '{"health":100,"mana":30,"fun":5,"fatigue":0,"money":100}' }
+      let(:new_file) {File.read('../valera/saves/test_save.json')}
       it {
         allow($stdin).to receive(:gets).and_return('test_save')
         Saver.save_game(status)
-        new_file = File.read('../valera/saves/test_save.json')
         expect(new_file).to eq file
       }
     end
@@ -32,9 +32,9 @@ RSpec.describe Saver do
           'money' => 100
         }
       end
+      let(:curr_status) {Saver.load_game}
       it {
         allow($stdin).to receive(:gets).and_return('test_save')
-        curr_status = Saver.load_game
         expect(curr_status).to eq status
       }
     end

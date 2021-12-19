6aeaf789c466a3fefb41745ef1f55acead340c70
Ant2000s <skrebnev.anton@gmail.com>
2021-12-19 13:56:19 +0700

    update test game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,35 +4,34 @@ require './lib/valera'
 require 'json'
 
 RSpec.describe Game do
-  actions = Actions.new
-  game = Game.new
-  file = File.read('../valera/configurate.json')
-  app = JSON.parse(file)
-  actions_array = actions.fetching_actions(app)
+  let(:actions) { Actions.new }
+  let(:game) { Game.new }
+  let(:file) { File.read('../valera/configurate.json') }
+  let(:app) { JSON.parse(file) }
+  let(:actions_array) { Actions.new.fetching_actions(app) }
+  let(:valera) { Valera.new }
 
   describe 'perform action!' do
     context 'action_item = 4' do
-      let(:status) do
-        {
-          'health' => 100,
-          'mana' => 100,
-          'fun' => 0,
-          'fatigue' => 0,
-          'money' => 0
-        }
-      end
-      let(:new_status) do
-        {
-          'health' => 100,
-          'mana' => 90,
-          'fun' => 1,
-          'fatigue' => 10,
-          'money' => 0
-        }
-      end
-      valera = Valera.new
-      game.action_item = 4
+      #let(:game.action_item) { 4 }
       it {
+        status =
+          {
+            'health' => 100,
+            'mana' => 100,
+            'fun' => 0,
+            'fatigue' => 0,
+            'money' => 0
+          }
+        new_status =
+          {
+            'health' => 100,
+            'mana' => 90,
+            'fun' => 1,
+            'fatigue' => 10,
+            'money' => 0
+          }
+        game.action_item = 4
         valera = game.perform_action!(status, app, actions_array)
         expect(valera.status).to eq new_status
       }
